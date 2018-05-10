5d171abea638bab097241a34c799b6dcdf738f6e
joosthoeks <joosthoeks@gmail.com>
2018-05-10 20:07:29 +0000

    modified

--- examples/coincap.py
+++ examples/coincap.py
@@ -8,6 +8,18 @@ def d(j):
 
 def main():
     api = cc.CoinCap()
+    d(api.get_coins())
+#    d(api.get_map())
+#    d(api.get_front())
+#    d(api.get_global())
+#    d(api.get_page('BTC'))
+#    d(api.get_history('BTC'))
+#    d(api.get_history_1day('BTC'))
+#    d(api.get_history_7day('BTC'))
+#    d(api.get_history_30day('BTC'))
+#    d(api.get_history_90day('BTC'))
+#    d(api.get_history_180day('BTC'))
+#    d(api.get_history_365day('BTC'))
 
 if __name__ == '__main__':
     main()
