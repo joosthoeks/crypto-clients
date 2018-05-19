4c4af7b2ffdf1e4a983b86ace20bd6587f126800
joosthoeks <joosthoeks@gmail.com>
2018-05-19 13:06:56 +0000

    modified

--- examples/coinmap.py
+++ examples/coinmap.py
@@ -8,6 +8,15 @@ def d(j):
 
 def main():
     api = cc.CoinMap()
+    d(api.venues(query='Pizza'))
+#    d(api.new_venue())
+#    d(api.read_venue(1))
+#    d(api.update_venue())
+#    d(api.delete_venue())
+#    d(api.comments(1))
+#    d(api.create_comment())
+#    d(api.ratings(1))
+#    d(api.create_rating())
 
 if __name__ == '__main__':
     main()
