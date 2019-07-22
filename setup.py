3b4b2bd8e80c8acd9d97cffffbef0033457ab49d
joosthoeks <joosthoeks@gmail.com>
2019-07-22 17:46:24 +0000

    updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crypto-clients',
-    version='20190721.0',
+    version='20190722.0',
     description='Python 3 API crypto-clients',
     keywords='python3 crypto clients',
     url='https://github.com/joosthoeks/crypto-clients',
@@ -11,15 +11,7 @@ setup(
     author_email='joosthoeks@gmail.com',
     license='GNU',
     packages=find_packages(),
-    install_requires=[
-#        'base64',
-#        'hashlib',
-#        'hmac',
-#        'json',
-#        'requests',
-#        'time',
-#        'urllib',
-    ],
+    install_requires=[],
     zip_safe=False
 )
 
