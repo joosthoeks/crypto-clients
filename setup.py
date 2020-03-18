fa2b4c1c8ec79d08f320ad0fda11a9fffc3c1b30
joosthoeks <joosthoeks@gmail.com>
2020-03-18 22:40:53 +0000

    updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crypto-clients',
-    version='20191108.0',
+    version='20200318.0',
     description='Python 3 API crypto-clients',
     keywords='python3 crypto clients',
     url='https://github.com/joosthoeks/crypto-clients',
